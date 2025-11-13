5d06ea5e2518d71eb50a3db9af094c1c9f11d251
Shivam Parashar <shivamparashar1213@gmail.com>
2025-11-13 22:18:34 +0530

    Refactor BaseTest to use @BeforeTest and @AfterTest

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,52 +2,29 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.Parameters;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.annotations.*;
 
 public class BaseTest {
+    protected WebDriver driver;
 
-	protected WebDriver driver; // accessible in child classes
+    @Parameters("browser")
+    @BeforeTest
+    public void launchByBrowserName(String browser) {
+        System.out.println("Launching browser: " + browser);
+        if (browser.equalsIgnoreCase("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "C:\\Tools\\chromedriver.exe");
+            driver = new ChromeDriver();
+        } else {
+            throw new IllegalArgumentException("Unsupported browser: " + browser);
+        }
+        driver.manage().window().maximize();
+    }
 
-	@BeforeMethod
-	@Parameters("browser")
-	public void launchByBrowserName(String browser) {
-		switch (browser.toLowerCase()) {
-		case "chrome":
-			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
-			break;
-		case "firefox":
-			WebDriverManager.firefoxdriver().setup();
-			driver = new FirefoxDriver();
-			break;
-		case "ie":
-			WebDriverManager.iedriver().setup();
-			driver = new InternetExplorerDriver();
-			break;
-		case "edge":
-			WebDriverManager.edgedriver().setup();
-			driver = new EdgeDriver();
-			break;
-		default:
-			throw new IllegalArgumentException("Browser not supported: " + browser);
-		}
-
-		driver.manage().window().maximize();
-		driver.get("https://www.google.com/");
-		System.out.println("🌐 Browser launched: " + browser);
-	}
-
-	@AfterMethod
-	public void exitBrowser() {
-		if (driver != null) {
-			driver.quit();
-			System.out.println("✅ Browser closed successfully.");
-		}
-	}
+    @AfterTest
+    public void closeBrowser() {
+        if (driver != null) {
+            driver.quit();
+            System.out.println("Browser closed successfully.");
+        }
+    }
 }
